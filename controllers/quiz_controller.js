aae5972b29f6a0e9397bfed882fa6697885a1986
Algomez <alejandro.lgomez@alumnos.upm.es>
2016-05-01 14:28:20 -0400

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,4 +1,5 @@
 
+
 var models = require('../models');
 var Sequelize = require('sequelize');
 
@@ -114,6 +115,20 @@ exports.update = function(req, res, next) {
       res.render('quizzes/edit', {quiz: req.quiz});
     })
     .catch(function(error) {
+	  req.flash('error', 'Error al editar el Quiz: '+error.message);
+      next(error);
+    });
+};
+
+
+// DELETE /quizzes/:id
+exports.destroy = function(req, res, next) {
+  req.quiz.destroy()
+    .then( function() {
+	  req.flash('success', 'Quiz borrado con éxito.');
+      res.redirect('/quizzes');
+    })
+    .catch(function(error){
 	  req.flash('error', 'Error al editar el Quiz: '+error.message);
       next(error);
     });
