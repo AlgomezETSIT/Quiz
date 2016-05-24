b114fdd21811b56421eff1eb2b79ce03161e4849
Algomez <alejandro.lgomez@alumnos.upm.es>
2016-05-24 05:15:27 -0400

    P13: Autores comentarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,7 +11,11 @@ var cloudinary_image_options = { crop: 'limit', width: 200, height: 200, radius:
 
 // Autoload el quiz asociado a :quizId
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.findById(quizId, {attributes: ['id', 'question', 'answer'], include: [ models.Comment, models.Attachment ] })
+  models.Quiz.findById(quizId, {attributes: ['id', 'question', 'answer', 'AuthorId'], 
+    include: [  {model: models.Comment, 
+      include: [  {model: models.User, as: 'Author', attributes: ['username']}]}, 
+                                models.Attachment, 
+                                {model: models.User, as: 'Author', attributes: ['username']} ] })
       .then(function(quiz) {
           if (quiz) {
             req.quiz = quiz;
@@ -68,7 +72,7 @@ exports.index = function(req, res, next) {
       });
   }
   else {
-    models.Quiz.findAll({ attributes: ['id', 'question', 'answer'], include: [ models.Attachment ] })
+    models.Quiz.findAll({ attributes: ['id', 'question', 'answer'], include: [ models.Attachment, {model: models.User, as: 'Author', attributes: ['username']} ] })
       .then(function(quizzes) {
         if (!req.params.format || req.params.format === "html") {
             res.render('quizzes/index.ejs', { quizzes: quizzes,
